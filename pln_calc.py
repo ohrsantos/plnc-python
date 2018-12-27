44d59571c545efca10d67d6d1554bf89c68b26a4
Orlando H. R. Santos <orlando.hrsantos@gmail.com>
2018-12-26 23:01:26 -0200

    "

--- pln_calc.py
+++ pln_calc.py
@@ -2,6 +2,6 @@ from stack import Stack
 
 class PlnCalc():
     def __init__(self):
-        self.reg_stack = Stack()
+        self.stack = Stack()
     def repush_last(self):
-        self.reg_stack.push(self.reg_stack.last())
+        self.stack.push(self.stack.last())
--- plnc.py
+++ plnc.py
@@ -1,3 +1,9 @@
+#ohrsantos
+from termcolor import colored, cprint
+from colorama import Fore, Back, Style
+from colorama import init
+init(autoreset=True)
+
 from stack import Stack
 from pln_calc import PlnCalc
 
@@ -7,12 +13,22 @@ print(__name__)
 
 
 calc = PlnCalc()
-calc.reg_stack.load([1,2,3,4])
-calc.reg_stack.push(3)
-print(calc.reg_stack.get())
-print(calc.reg_stack.size())
+calc.stack.load([1,2,3,4])
+calc.stack.push(3)
+print(calc.stack.get())
+print(calc.stack.size())
 
 
 calc.repush_last()
-print(calc.reg_stack.get())
-print(calc.reg_stack.size())
+print(calc.stack.get())
+print(calc.stack.size())
+print("\033[38;5;1mThis is a text!\033[0m")
+
+print(colored('hello', 'red'), colored('world', 'green'))
+cprint('Hello, World!', 'green', 'on_red')
+
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Fore.BLUE + Back.WHITE + 'Orlando')
+print('back to normal now')
