bb44f969ae138711acd465c7feea506aa63a5af7
Orlando H. R. Santos <orlando.hrsantos@gmail.com>
2018-12-27 11:48:21 -0200

    .

--- pln_calc.py
+++ pln_calc.py
@@ -1,7 +1,19 @@
 from stack import Stack
 
 class PlnCalc():
-    def __init__(self):
-        self.stack = Stack()
-    def repush_last(self):
-        self.stack.push(self.stack.last())
+   def __init__(self):
+       self.stack = Stack()
+       self.input_register = 0.0
+   def repush_last(self):
+       self.stack.push(self.stack.last())
+   def enter(self, value):
+       if value != 0.0:
+          self.stack.push(value) 
+       else:
+          repush_last()
+   def sum(self, value):
+       if type(value) == str:
+          self.stack.push(self.stack.pop() +  self.stack.pop())
+       else:
+          self.stack.push(value + self.stack.pop())
+        
